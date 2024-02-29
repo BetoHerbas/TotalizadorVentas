ce5cf38c299df6171345c753ac044637cb87ee41
BetoHerbas <betitoherbas@gmail.com>
2024-02-29 11:59:54 -0400

    show the net price plus taxes of the state NV, 8.0%

--- src/totalizador.js
+++ src/totalizador.js
@@ -2,7 +2,14 @@ function calculateNetPrice(price, quantity) {
   return price * quantity;
 }
 function calculateTaxes(netPrice, state){
-  return +(netPrice * 0.0825).toFixed(3);
+  switch (state) {
+    case "CA":
+      return +(netPrice * 0.0825).toFixed(3);
+    case "NV":
+      return +(netPrice * 0.08).toFixed(3);
+    default:
+      return 0;
+  }
 }
 function calculateTote (price, quantity, state) {
   let netPrice = calculateNetPrice(price, quantity)
--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -17,4 +17,7 @@ describe("Price plus taxes of state", () => {
   it("should return the net price plus taxes of the state CA, 8.25%", () => {
     expect(calculateTote(5, 10, "CA")).toEqual(54.125);
   });
+  it("should return the net price plus taxes of the state NV, 8.0%", () => {
+    expect(calculateTote(100, 1, "NV")).toEqual(108);
+  });
 });
